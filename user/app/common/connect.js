0c24cb6b44661281497b794ecbdba4d9de41808d
Khanh Tuong Vang <vangkhanhtuong7743@gmail.com>
2022-12-17 15:51:36 +0700

    Create connection function with mongoose

--- user/app/common/connect.js
+++ user/app/common/connect.js
@@ -1,20 +1,31 @@
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'demo_node_api'
-});
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'demo_node_api'
+// });
 
-connection.connect(function(err) {
-    if (err) {
-        console.log('Connection error: ' + err.message);
+// connection.connect(function(err) {
+//     if (err) {
+//         console.log('Connection error: ' + err.message);
 
-    } else {
-        console.log('Connection success!');
+//     } else {
+//         console.log('Connection success!');
+//     }
+// });
+
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+
+async function connection(){
+    try{
+        await mongoose.connect('mongodb://127.0.0.1:27017/hotel_dev')
+        console.log('Connect Successfully')
+    }catch(error){
+        console.log('Connect Failed')
     }
-});
+}
 
 module.exports = connection;
-//check dif
