f3ac1dacd98cc141fa55c6279882b0552ca5f180
honam <75104291+honambsn@users.noreply.github.com>
2022-12-26 23:23:22 +0700

    delete trash

--- hotel-management-nam/user/app/routes/account.router.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = function(router) {
-    var userController = require('../controllers/user.controller');
-    //var JWT = require('../common/_JWT');
-    router.post('/account/login', userController.login);
-
-    router.post('/account/add', userController.add_user);
-
-}
